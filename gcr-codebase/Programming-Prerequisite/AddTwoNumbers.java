7ef569f71cc5177d6116aef2a7b927d986cd8ff6
Aryan <borkararyan03@gmail.com>
2025-12-16 16:52:54 +0530

    [Aryan] : added initial setup

---
+++ gcr-codebase/Programming-Prerequisite/AddTwoNumbers.java
@@ -0,0 +1,13 @@
+java.util.Scanner;
+public class AddTwoNumbers {
+
+    public static void main(String[] args) {
+	Scanner sc  = new Scanner(System.in);
+	    int a,b;
+		a = sc.nextint;
+		b = sc.nextint;
+		int sum = a+b;
+        System.out.println(sum);
+        
+    }
+}
