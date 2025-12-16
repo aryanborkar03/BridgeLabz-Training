7d324cd2779bf1f0246f288a1740d6560c691f3e
Aryan <borkararyan03@gmail.com>
2025-12-16 22:25:42 +0530

    [Aryan : added CalculateSimpleIntrest]

--- gcr-codebase/Programming-Prerequisite/CalculateSimpleInterest.java
+++ gcr-codebase/Programming-Prerequisite/CalculateSimpleInterest.java
@@ -1,13 +1,13 @@
-import.java.util.Scanner;
+import java.util.Scanner;
 public class CalculateSimpleInterest{
-    public static void main(String[] args)
+    public static void main(String[] args){
 	Scanner sc = new Scanner(System.in);
 	int principal;
-	principal = sc.nextint;
+	principal = sc.nextInt();
 	int rate;
-	rate = sc.nextint;
+	rate = sc.nextInt();
 	int time;
-	time = sc.nextint;
+	time = sc.nextInt();
 	int intrest = (principal*rate*time)/100;
-	System.out.println(intrest);
+	System.out.println(intrest);}
 }
