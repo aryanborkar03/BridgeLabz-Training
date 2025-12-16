fa925e3441dcd813dbd0d6474387f0ad14839ff4
Aryan <borkararyan03@gmail.com>
2025-12-16 22:32:01 +0530

    [Aryan : added KilometreToMile]

---
+++ gcr-codebase/Programming-Prerequisite/KilometerToMiles.java
@@ -0,0 +1,16 @@
+import java.util.Scanner;
+
+public class KilometerToMiles {
+    public static void main(String[] args) {
+
+        Scanner sc = new Scanner(System.in);
+
+        double kilometers;
+        kilometers = sc.nextDouble();
+
+        double miles = kilometers * 0.621371;
+        System.out.println(miles);
+
+        sc.close();
+    }
+}
