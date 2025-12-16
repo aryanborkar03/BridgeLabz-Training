ac9341403a18a466d11344e64a403ef371b828d1
Aryan <borkararyan03@gmail.com>
2025-12-16 17:04:00 +0530

    [Aryan] : added AreaofaCircle

---
+++ gcr-codebase/Programming-Prerequisite/AreaofaCircle.java
@@ -0,0 +1,10 @@
+java.util.Scanner;
+public class AreaofaCircle {
+    public static void main(String[] args)
+	Scanner sc = new Scanner(System.in);
+	int r;
+	r = sc.nextint;
+	int area = (3.14*(r*r));
+	System.out.println(area);
+	
+}
