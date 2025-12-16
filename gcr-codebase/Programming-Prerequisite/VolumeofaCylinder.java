1c5e5be8f12354bcf4d9dd64266ff8db5096b780
Aryan <borkararyan03@gmail.com>
2025-12-16 22:29:50 +0530

    [Aryan : added VolumeofCylinder]

--- gcr-codebase/Programming-Prerequisite/VolumeofaCylinder.java
+++ gcr-codebase/Programming-Prerequisite/VolumeofaCylinder.java
@@ -1,12 +1,12 @@
-java.util.Scanner;
+import java.util.Scanner;
 public class VolumeofaCylinder {
-    public static void main(String[] args)
+    public static void main (String[] args){
 	Scanner sc = new Scanner(System.in);
-	int r;
-	int h;
-	r = sc.nextint;
-	h = sc.nextint;
-	int volume = (3.14*(r*r)*h);
-	System.out.println(volume);
+	double r;
+	double h;
+	r = sc.nextInt();
+	h = sc.nextInt();
+	double volume = (3.14*(r*r)*h);
+	System.out.println(volume);}
 	
 }
