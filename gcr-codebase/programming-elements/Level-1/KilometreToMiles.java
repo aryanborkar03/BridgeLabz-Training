0d5fab955d2f4d2647b1cb41b1365a823df67cbf
Aryan <borkararyan03@gmail.com>
2025-12-17 23:05:42 +0530

    [Aryan added : KilometreToMiles.java]

--- gcr-codebase/programming-elements/Level-1/KilometreToMiles.java
+++ gcr-codebase/programming-elements/Level-1/KilometreToMiles.java
@@ -2,7 +2,7 @@ public class KilometreToMiles{
     public static void main(String args[])
 	{
 	  //just to convert km to miles 1km = 16 miles
-	  
+	   
 	  double kilometres = 10.8;
 	  double miles  = (10.8*0.6);
 	  
