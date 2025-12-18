599fd22fe27e9d5882362a9239c49869b3526e78
Aryan <borkararyan03@gmail.com>
2025-12-18 11:00:52 +0530

    [Aryan : added VolumeOfEarth]

--- gcr-codebase/programming-elements/Level-1/VolumeOfEarth.java
+++ gcr-codebase/programming-elements/Level-1/VolumeOfEarth.java
@@ -1,10 +1,10 @@
 public class VolumeOfEarth{
-    public static void main(String[]args){  
+    public static void main(String[]args){
 		
 		//to print the volume of earth and doing appropriate conversions
         int radiusOfEarth=6378;
         double volumeofearthincubickm=(4/3)*3.14*radiusOfEarth*radiusOfEarth*radiusOfEarth;
-        double volumeofearthincubicmiles=volumeofearthincubickm/1.6;
+        double volumeofearthincubicmiles=volumeofearthincubickm/(1.6*1.6*1.6);
 		
 		//to print volume of earth in volumeofearthincubickm
         System.out.println("The volume of earth in cubic kilometers is "+volumeofearthincubickm+" and and cubic miles is "+volumeofearthincubicmiles);
