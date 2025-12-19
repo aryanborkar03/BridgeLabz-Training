5e889509f4649b17dc122f187f678423fbfb9b11
Aryan <borkararyan03@gmail.com>
2025-12-19 16:18:52 +0530

    [Aryan : added EmployeeBonus.java]

---
+++ gcr-codebase/control-flows/Level-2/EmployeeBonus.java
@@ -0,0 +1,19 @@
+//Created class Named EmployeeBonus to calculate the bonus amount
+import java.util.Scanner;
+public class EmployeeBonus{
+   public static void main(String[]args){
+
+    //take salary and years of service as input variables
+    Scanner sc=new Scanner(System.in);
+    double salary=sc.nextDouble();
+    int years=sc.nextInt();
+
+    if(years>5){
+        double bonus=salary*0.05;
+        System.out.println("Bonus amount is " + bonus);
+    }
+    else{
+        System.out.println("No bonus applicable");
+    }
+   }
+}
