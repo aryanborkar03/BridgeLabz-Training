e265e971c58270f6d73adb09b718e69b19e08df7
Aryan <borkararyan03@gmail.com>
2025-12-22 01:44:55 +0530

    [Aryan : added TriangleParkRun.java]

---
+++ gcr-codebase/Methods/TriangleParkRun.java
@@ -0,0 +1,20 @@
+//Created class to calculate number of rounds in triangular park
+import java.util.Scanner;
+public class TriangleParkRun {
+
+    public static double calculateRounds(double a,double b,double c){
+        double perimeter = a + b + c;
+        return 5000 / perimeter;
+    }
+   
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+
+        double side1 = sc.nextDouble();
+        double side2 = sc.nextDouble();
+        double side3 = sc.nextDouble();
+
+        double rounds = calculateRounds(side1,side2,side3);
+        System.out.println(rounds);
+    }
+}
