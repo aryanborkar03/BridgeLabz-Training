5f2e33c13a331aca6619b3cc434a99eb4160298e
Aryan <borkararyan03@gmail.com>
2025-12-22 01:51:34 +0530

    [Aryan added : LeapYearCheck.java]

---
+++ gcr-codebase/Methods/Level-2/LeapYearCheck.java
@@ -0,0 +1,20 @@
+//Created class to check leap year
+import java.util.Scanner;
+public class LeapYearCheck {
+
+    public static boolean isLeapYear(int year){
+        if(year<1582)
+            return false;
+        return (year%4==0 && year%100!=0) || (year%400==0);
+    }
+
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int year = sc.nextInt();
+
+        if(isLeapYear(year))
+            System.out.println("Leap Year");
+        else
+            System.out.println("Not a Leap Year");
+    }
+}
