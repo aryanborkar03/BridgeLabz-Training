3161b19f67eff4f6bdde7c821bb7f4da2abe28b7
Aryan <borkararyan03@gmail.com>
2025-12-22 09:53:27 +0530

    [Aryan : added MaximumHandshakes.java]

---
+++ gcr-codebase/Methods/Level-1/MaximumHandshakes.java
@@ -0,0 +1,16 @@
+//Created class to calculate maximum handshakes
+import java.util.Scanner;
+public class MaximumHandshakes {
+
+    public static int calculateHandshakes(int n){
+        return (n * (n - 1)) / 2;
+    }
+
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int numberOfStudents = sc.nextInt();
+
+        int handshakes = calculateHandshakes(numberOfStudents);
+        System.out.println(handshakes);
+    }
+}
