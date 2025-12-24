c765b00cd3a5c36bc5e39d31b3d136e3d19e5f31
Aryan <borkararyan03@gmail.com>
2025-12-24 12:56:42 +0530

    [Aryan : added StringIndexOutOfBoundsException.java]

---
+++ gcr-codebase/Strings/Level-1/StringIndexOutOfBoundsException.java
@@ -0,0 +1,32 @@
+//Created class Named StringIndexOutOfBoundsException
+import java.util.Scanner;
+public class StringIOB{
+   public static void main(String[]args){
+
+    Scanner input=new Scanner(System.in);
+
+    //take input string
+    String text=input.next();
+
+    //call method to handle StringIOB
+    handleException(text);
+   }
+
+   //method to generate StringIOB
+   public static void generateException(String text){
+
+       //this will generate StringIOB
+       System.out.println(text.charAt(text.length()));
+   }
+
+   //method to handle StringIOB 
+   public static void handleException(String text){
+
+       try{
+           generateException(text);
+       }
+       catch(StringIndexOutOfBoundsException e){
+           System.out.println("StringIndexOutOfBoundsException handled successfully");
+       }
+   }
+}
