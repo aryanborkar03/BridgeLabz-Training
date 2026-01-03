6edd1d42a1774b4bbb9347404932b0ed12df5c41
Aryan <borkararyan03@gmail.com>
2026-01-03 16:49:03 +0530

    [Aryan added : Driver.java]

---
+++ java-oops-practice/scenario-based-codebase/oops_scenario_based/src/com/cabbygo/Driver.java
@@ -0,0 +1,23 @@
+package com.cabbygo;
+public class Driver {
+
+    private String name;
+    private String licenseNumber;
+    private double rating;   // encapsulated
+
+    public Driver(String name, String licenseNumber, double rating) {
+        this.name = name;
+        this.licenseNumber = licenseNumber;
+        this.rating = rating;
+    }
+
+    public String getName() { return name; }
+    public String getLicenseNumber() { return licenseNumber; }
+    public double getRating() { return rating; }
+
+    public void showDriverInfo() {
+        System.out.println("Driver: " + name +
+                " | License: " + licenseNumber +
+                " | Rating: " + rating);
+    }
+}
