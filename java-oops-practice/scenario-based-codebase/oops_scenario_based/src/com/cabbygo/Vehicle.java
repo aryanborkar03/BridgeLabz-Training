2b2e64da57f8381b2b6045b59bcfbd7061b15528
Aryan <borkararyan03@gmail.com>
2026-01-05 17:05:03 +0530

    [Aryan added : Vehicle.java]

--- java-oops-practice/scenario-based-codebase/oops_scenario_based/src/com/cabbygo/Vehicle.java
+++ java-oops-practice/scenario-based-codebase/oops_scenario_based/src/com/cabbygo/Vehicle.java
@@ -13,9 +13,12 @@ public abstract class Vehicle {
         this.ratePerKm = ratePerKm;
     }
 
-    public String getVehicleNumber() { return vehicleNumber; }
-    public int getCapacity() { return capacity; }
-    public String getType() { return type; }
+    public String getVehicleNumber() 
+    { return vehicleNumber; }
+    public int getCapacity() 
+    { return capacity; }
+    public String getType() 
+    { return type; }
 
     public abstract void showVehicleInfo();
 }
