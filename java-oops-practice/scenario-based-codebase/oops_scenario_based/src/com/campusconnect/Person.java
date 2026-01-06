b3847698f20623040a7960eed4861ec313299454
Aryan <borkararyan03@gmail.com>
2026-01-06 13:20:40 +0530

    [Aryan added : Person.java]

---
+++ java-oops-practice/scenario-based-codebase/oops_scenario_based/src/com/campusconnect/Person.java
@@ -0,0 +1,15 @@
+package com.campusconnect;
+
+public abstract class Person {
+    protected String name;
+    protected String email;
+    protected String id;
+
+    public Person(String name, String email, String id) {
+        this.name = name;
+        this.email = email;
+        this.id = id;
+    }
+
+    public abstract void printDetails();
+}
