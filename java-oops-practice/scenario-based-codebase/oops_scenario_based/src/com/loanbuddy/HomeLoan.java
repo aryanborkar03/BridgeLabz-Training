f8dd1ec2374278a167e088ea912aba59a7aa69b6
Aryan <borkararyan03@gmail.com>
2026-01-07 23:52:26 +0530

    [Aryan added : HomeLoan.java]

---
+++ java-oops-practice/scenario-based-codebase/oops_scenario_based/src/com/loanbuddy/HomeLoan.java
@@ -0,0 +1,8 @@
+package com.loanbuddy;
+
+public class HomeLoan extends LoanApplication {
+
+    public HomeLoan(Applicant applicant, double interestRate, int termMonths) {
+        super(applicant, interestRate, termMonths);
+    }
+}
