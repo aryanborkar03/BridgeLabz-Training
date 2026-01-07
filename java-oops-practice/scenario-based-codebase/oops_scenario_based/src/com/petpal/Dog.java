f5082f823ad0719a3052a1e9c569437d5322713c
Aryan <borkararyan03@gmail.com>
2026-01-08 00:01:52 +0530

    [Aryan added : Dog.java]

---
+++ java-oops-practice/scenario-based-codebase/oops_scenario_based/src/com/petpal/Dog.java
@@ -0,0 +1,12 @@
+package com.petpal;
+
+public class Dog extends Pet {
+    public Dog(String name, int age) {
+        super(name, "Dog", age);
+    }
+
+    @Override
+    public void makeSound() {
+        System.out.println(name + " says: Woof! Woof!");
+    }
+}
