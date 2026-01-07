b23b1972bd1cc0908a74010c8ba43604d3e5e7fc
Aryan <borkararyan03@gmail.com>
2026-01-07 23:45:04 +0530

    [Aryan added : Cat.java]

---
+++ java-oops-practice/scenario-based-codebase/oops_scenario_based/src/com/petpal/Cat.java
@@ -0,0 +1,12 @@
+package com.petpal;
+
+public class Cat extends Pet {
+    public Cat(String name, int age) {
+        super(name, "Cat", age);
+    }
+
+    @Override
+    public void makeSound() {
+        System.out.println(name + " says: Meowww!");
+    }
+}
