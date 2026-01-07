ea9b659ed2f3469791af4f1aeda3e971caa0a9d4
Aryan <borkararyan03@gmail.com>
2026-01-07 23:51:24 +0530

    [Aryan added : AutoLoan.java]

---
+++ java-oops-practice/scenario-based-codebase/oops_scenario_based/src/com/loanbuddy/AutoLoan.java
@@ -0,0 +1,8 @@
+package com.loanbuddy;
+
+public class AutoLoan extends LoanApplication {
+
+    public AutoLoan(Applicant applicant, double interestRate, int termMonths) {
+        super(applicant, interestRate, termMonths);
+    }
+}
