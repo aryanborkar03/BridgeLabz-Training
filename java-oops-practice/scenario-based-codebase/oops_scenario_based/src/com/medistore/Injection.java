f9ce8fb77003fc50a1e11aa074b7459498a1856b
Aryan <borkararyan03@gmail.com>
2026-01-08 15:49:58 +0530

    [Aryan added : Injection.java]

---
+++ java-oops-practice/scenario-based-codebase/oops_scenario_based/src/com/medistore/Injection.java
@@ -0,0 +1,14 @@
+package com.medistore;
+import java.time.LocalDate;
+
+public class Injection extends Medicine {
+
+    public Injection(String name, double price, LocalDate expiryDate) {
+        super(name, price, expiryDate);
+    }
+
+    @Override
+    public boolean checkExpiry() {
+        return LocalDate.now().isAfter(expiryDate.minusDays(10));
+    }
+}
