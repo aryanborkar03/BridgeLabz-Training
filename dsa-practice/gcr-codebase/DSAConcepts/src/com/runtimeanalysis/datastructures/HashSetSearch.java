0811a7245799070ec0be67c9ca9b698814f978c2
Aryan <borkararyan03@gmail.com>
2026-01-09 18:58:24 +0530

    [Aryan added : HashSetSearch.java]

---
+++ dsa-practice/gcr-codebase/DSAConcepts/src/com/runtimeanalysis/datastructures/HashSetSearch.java
@@ -0,0 +1,12 @@
+package com.runtimeanalysis.datastructures;
+
+
+import java.util.HashSet;
+import java.util.Set;
+
+public class HashSetSearch {
+
+	public static boolean search(Set<Integer> set, int target) {
+		return set.contains(target);
+	}
+}
