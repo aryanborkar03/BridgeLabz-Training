b9d67c5cd14bebce0139965ff3c7204335cbc960
Aryan <borkararyan03@gmail.com>
2026-01-09 18:57:05 +0530

    [Aryan added : StringConcatenation.java]

---
+++ dsa-practice/gcr-codebase/DSAConcepts/src/com/runtimeanalysis/stringconcatenation/StringConcatenation.java
@@ -0,0 +1,11 @@
+package com.runtimeanalysis.stringconcatenation;
+
+public class StringConcatenation {
+    public static String concatenate(int count) {
+        String result = "";
+        for (int i = 0; i < count; i++) {
+            result = result + "A";
+        }
+        return result;
+    }
+}
