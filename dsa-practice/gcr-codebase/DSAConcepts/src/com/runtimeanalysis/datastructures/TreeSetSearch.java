f02b4b37d910676f580fa85613aaa7a7ca277b9b
Aryan <borkararyan03@gmail.com>
2026-01-09 18:58:56 +0530

    [Aryan added : TreeSetSearch.java]

---
+++ dsa-practice/gcr-codebase/DSAConcepts/src/com/runtimeanalysis/datastructures/TreeSetSearch.java
@@ -0,0 +1,11 @@
+package com.runtimeanalysis.datastructures;
+
+
+import java.util.Set;
+
+public class TreeSetSearch {
+	public static boolean search(Set<Integer> set, int target) {
+		return set.contains(target);
+	}
+}
+	
