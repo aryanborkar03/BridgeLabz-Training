3aeb7d167a8c392a5537aa17622410a62a67bdce
Aryan <borkararyan03@gmail.com>
2026-01-10 18:51:13 +0530

    [Aryan added : InternationalTrip.java]

---
+++ java-oops-practice/scenario-based-codebase/oops_scenario_based/src/com/tourmate/InternationalTrip.java
@@ -0,0 +1,20 @@
+package com.tourmate;
+class InternationalTrip extends Trip {
+
+    public InternationalTrip(String destination, int duration,
+                             Transport transport, Hotel hotel, Activity activity) {
+        super(destination, duration, transport, hotel, activity);
+    }
+
+    @Override
+    public void book() {
+        System.out.println("Booking international trip to " + destination);
+        System.out.println("Passport & visa verification required.");
+        System.out.println("Total Budget: ₹" + getTotalBudget());
+    }
+
+    @Override
+    public void cancel() {
+        System.out.println("International trip cancelled with refund rules.");
+    }
+}
