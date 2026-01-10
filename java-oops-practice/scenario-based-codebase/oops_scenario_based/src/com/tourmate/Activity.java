23d7dd3610b9eade0db1bb66da3d357a4a830860
Aryan <borkararyan03@gmail.com>
2026-01-10 18:47:31 +0530

    [Aryan added : Activity.java]

---
+++ java-oops-practice/scenario-based-codebase/oops_scenario_based/src/com/tourmate/Activity.java
@@ -0,0 +1,12 @@
+package com.tourmate;
+class Activity {
+    private double cost;
+
+    public Activity(double cost) {
+        this.cost = cost;
+    }
+
+    public double getCost() {
+        return cost;
+    }
+}
