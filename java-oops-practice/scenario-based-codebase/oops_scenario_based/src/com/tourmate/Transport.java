c1a23e66b8c8de1fd5621de6d5cc5c65dfb7401e
Aryan <borkararyan03@gmail.com>
2026-01-10 18:52:49 +0530

    [Aryan added : Transport.java]

---
+++ java-oops-practice/scenario-based-codebase/oops_scenario_based/src/com/tourmate/Transport.java
@@ -0,0 +1,12 @@
+package com.tourmate;
+class Transport {
+    private double cost;
+
+    public Transport(double cost) {
+        this.cost = cost;
+    }
+
+    public double getCost() {
+        return cost;
+    }
+}
