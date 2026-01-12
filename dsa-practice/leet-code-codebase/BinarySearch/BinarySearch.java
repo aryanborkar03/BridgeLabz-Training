e9751eddf9ffe1a66c54ad93d6b8d96b4c8ec118
Aryan <borkararyan03@gmail.com>
2026-01-12 17:26:16 +0530

    [Aryan added : BinarySearch.java]

---
+++ dsa-practice/leet-code-codebase/BinarySearch/BinarySearch.java
@@ -0,0 +1,44 @@
+import java.util.Scanner;
+
+class BinarySearch {
+
+    public int search(int[] nums, int target) {
+        int left = 0;
+        int right = nums.length - 1;
+
+        while (left <= right) {
+            int mid = left + (right - left) / 2;
+
+            if (nums[mid] == target) {
+                return mid;
+            } else if (nums[mid] < target) {
+                left = mid + 1;
+            } else {
+                right = mid - 1;
+            }
+        }
+
+        return -1;
+    }
+
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        Solution obj = new Solution();
+
+        System.out.print("Enter size of array: ");
+        int n = sc.nextInt();
+
+        int[] nums = new int[n];
+        System.out.println("Enter sorted array elements:");
+        for (int i = 0; i < n; i++) {
+            nums[i] = sc.nextInt();
+        }
+
+        System.out.print("Enter target element: ");
+        int target = sc.nextInt();
+
+        int result = obj.search(nums, target);
+
+        System.out.println("Result index: " + result);
+    }
+}
