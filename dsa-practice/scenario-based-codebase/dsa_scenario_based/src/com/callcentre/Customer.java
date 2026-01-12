4efe7b5a8b57493eb1d646d97e9a8719a5ab3543
Aryan <borkararyan03@gmail.com>
2026-01-12 13:14:48 +0530

    [Aryan added : Customer.java]

---
+++ dsa-practice/scenario-based-codebase/dsa_scenario_based/src/com/callcentre/Customer.java
@@ -0,0 +1,12 @@
+package com.callcentre;
+
+public class Customer {
+     String name;
+     boolean VIP;
+     int callTime;
+     Customer(String name, boolean VIP , int callTime){
+    	 this.name = name;
+    	 this.VIP = VIP;
+    	 this.callTime = callTime;
+     }
+}
