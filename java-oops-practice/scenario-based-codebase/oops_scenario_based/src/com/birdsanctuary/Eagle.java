4b531661e6422fc8322186db97878567b1cc20cf
Aryan <borkararyan03@gmail.com>
2026-01-15 13:10:16 +0530

    [Aryan added : Eagle.java]

---
+++ java-oops-practice/scenario-based-codebase/oops_scenario_based/src/com/birdsanctuary/Eagle.java
@@ -0,0 +1,11 @@
+package com.birdsanctuary;
+public class Eagle extends Bird implements Flyable {
+
+    public Eagle(String id, String name) {
+        super(id, name, "Eagle");
+    }
+
+    public void fly() {
+        System.out.println(name + " is flying high");
+    }
+}
