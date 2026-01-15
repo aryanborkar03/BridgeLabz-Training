d073c82091ac999750efdc70d7a9dcd40cb72a3c
Aryan <borkararyan03@gmail.com>
2026-01-15 20:32:36 +0530

    [Aryan added : Student.java]

---
+++ dsa-practice/scenario-based-codebase/dsa_scenario_based/src/com/examcell/Student.java
@@ -0,0 +1,10 @@
+package com.examcell;
+public class Student {
+    String name;
+    int score;
+
+    public Student(String name, int score) {
+        this.name = name;
+        this.score = score;
+    }
+}
