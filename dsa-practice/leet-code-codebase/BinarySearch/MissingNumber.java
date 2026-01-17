aef7d8975613d02f5f5600174bf4fc1b9cc506de
Aryan <borkararyan03@gmail.com>
2026-01-17 12:37:01 +0530

    [Aryan added : MissingNumber.java]

---
+++ dsa-practice/leet-code-codebase/BinarySearch/MissingNumber.java
@@ -0,0 +1,28 @@
+import java.util.*;
+
+public class MissingNumber {
+
+    public static int missingNumber(int[] nums) {
+        int n = nums.length;
+        int expectedSum = n * (n + 1) / 2;
+        int actualSum = 0;
+
+        for (int num : nums) {
+            actualSum += num;
+        }
+
+        return expectedSum - actualSum;
+    }
+
+    public static void main(String[] args) {
+
+        int[] nums1 = {3, 0, 1};
+        System.out.println(missingNumber(nums1));
+
+        int[] nums2 = {0, 1};
+        System.out.println(missingNumber(nums2));
+
+        int[] nums3 = {9, 6, 4, 2, 3, 5, 7, 0, 1};
+        System.out.println(missingNumber(nums3));
+    }
+}
