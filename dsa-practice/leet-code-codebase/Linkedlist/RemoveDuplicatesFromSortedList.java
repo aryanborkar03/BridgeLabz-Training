16355cefe4a57b4ed3353cc006f43fca7ee6a3fb
Aryan <borkararyan03@gmail.com>
2026-01-17 12:38:05 +0530

    [Aryan added : RemoveDuplicatesFromSortedList.java]

---
+++ dsa-practice/leet-code-codebase/Linkedlist/RemoveDuplicatesFromSortedList.java
@@ -0,0 +1,42 @@
+class ListNode {
+    int val;
+    ListNode next;
+
+    ListNode(int val) {
+        this.val = val;
+    }
+}
+
+public class RemoveDuplicatesFromSortedList {
+
+    static ListNode deleteDuplicates(ListNode head) {
+        ListNode curr = head;
+
+        while (curr != null && curr.next != null) {
+            if (curr.val == curr.next.val) {
+                curr.next = curr.next.next;
+            } else {
+                curr = curr.next;
+            }
+        }
+        return head;
+    }
+
+    static void printList(ListNode head) {
+        while (head != null) {
+            System.out.print(head.val + " ");
+            head = head.next;
+        }
+    }
+
+    public static void main(String[] args) {
+        ListNode head = new ListNode(1);
+        head.next = new ListNode(1);
+        head.next.next = new ListNode(2);
+        head.next.next.next = new ListNode(3);
+        head.next.next.next.next = new ListNode(3);
+
+        head = deleteDuplicates(head);
+        printList(head);
+    }
+}
