2ec15a6b41069061f89c3207ab85e517ddf8ccda
Aryan <borkararyan03@gmail.com>
2026-01-18 23:17:34 +0530

    [Aryan : added LibraryManager.java]

---
+++ dsa-practice/scenario-based-codebase/dsa_scenario_based/src/com/smartlibrary/LibraryManager.java
@@ -0,0 +1,28 @@
+package com.smartlibrary;
+public class LibraryManager {
+
+    // Insertion Sort to keep books alphabetically sorted
+    public static void insertionSort(Book[] books, int size) {
+        for (int i = 1; i < size; i++) {
+            Book key = books[i];
+            int j = i - 1;
+
+            while (j >= 0 && books[j].title.compareToIgnoreCase(key.title) > 0) {
+                books[j + 1] = books[j];
+                j--;
+            }
+            books[j + 1] = key;
+        }
+    }
+
+    public static void showBooks(Book[] books, int size) {
+        if (size == 0) {
+            System.out.println("No borrowed books");
+            return;
+        }
+
+        for (int i = 0; i < size; i++) {
+            System.out.println((i + 1) + ". " + books[i].title);
+        }
+    }
+}
