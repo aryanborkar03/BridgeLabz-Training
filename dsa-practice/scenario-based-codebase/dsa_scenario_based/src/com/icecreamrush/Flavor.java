b6622c272ab9e641a1bc8cebe5ed50059c739b66
Aryan <borkararyan03@gmail.com>
2026-01-18 23:09:56 +0530

    [Aryan added : Flavor.java]

---
+++ dsa-practice/scenario-based-codebase/dsa_scenario_based/src/com/icecreamrush/Flavor.java
@@ -0,0 +1,10 @@
+package com.icecreamrush;
+public class Flavor {
+    String name;
+    int sales;
+
+    public Flavor(String name, int sales) {
+        this.name = name;
+        this.sales = sales;
+    }
+}
