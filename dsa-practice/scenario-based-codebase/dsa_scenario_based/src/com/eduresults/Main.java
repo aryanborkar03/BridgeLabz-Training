477448af83bab38fe606018b6159a588a8543572
Aryan <borkararyan03@gmail.com>
2026-01-19 15:41:20 +0530

    [Aryan added : Main.java]

--- dsa-practice/scenario-based-codebase/dsa_scenario_based/src/com/eduresults/Main.java
+++ dsa-practice/scenario-based-codebase/dsa_scenario_based/src/com/eduresults/Main.java
@@ -5,7 +5,7 @@ public class Main {
     public static void main(String[] args) {
 
         Scanner sc = new Scanner(System.in);
-
+  
         System.out.println("=================================");
         System.out.println(" EduResults – State Rank Generator ");
         System.out.println("=================================");
