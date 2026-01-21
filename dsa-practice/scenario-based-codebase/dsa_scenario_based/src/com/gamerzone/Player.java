81d446813e6cc7d4a127c42e1738ca0460272a86
Aryan <borkararyan03@gmail.com>
2026-01-21 16:25:36 +0530

    [Aryan : added Player.java]

---
+++ dsa-practice/scenario-based-codebase/dsa_scenario_based/src/com/gamerzone/Player.java
@@ -0,0 +1,11 @@
+package com.gamerzone;
+
+public class Player {
+    String name;
+    int score;
+
+    Player(String name, int score) {
+        this.name = name;
+        this.score = score;
+    }
+}
