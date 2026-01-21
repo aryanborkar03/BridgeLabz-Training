eeecdf82f4bf38dd8083f99c3726a18e56116448
Aryan <borkararyan03@gmail.com>
2026-01-21 16:19:33 +0530

    [Aryan added : Order.java]

---
+++ dsa-practice/scenario-based-codebase/dsa_scenario_based/src/com/tailorshop/Order.java
@@ -0,0 +1,10 @@
+package com.tailorshop;
+public class Order {
+    String customerName;
+    int deadlineDays;
+
+    public Order(String customerName, int deadlineDays) {
+        this.customerName = customerName;
+        this.deadlineDays = deadlineDays;
+    }
+}
