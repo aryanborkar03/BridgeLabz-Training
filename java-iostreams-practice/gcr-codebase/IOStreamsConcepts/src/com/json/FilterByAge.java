469b9d14b9e478e9d8f34a715419496294505980
Aryan <borkararyan03@gmail.com>
2026-01-30 17:20:52 +0530

    [Aryan added : FilterByAge]

---
+++ java-iostreams-practice/gcr-codebase/IOStreamsConcepts/src/com/json/FilterByAge.java
@@ -0,0 +1,19 @@
+package com.json;
+
+import com.fasterxml.jackson.databind.JsonNode;
+import com.fasterxml.jackson.databind.ObjectMapper;
+
+public class FilterByAge {
+    public static void main(String[] args) throws Exception {
+        String json = "[{\"name\":\"A\",\"age\":20},{\"name\":\"B\",\"age\":30}]";
+
+        ObjectMapper mapper = new ObjectMapper();
+        JsonNode array = mapper.readTree(json);
+
+        for (JsonNode node : array) {
+            if (node.get("age").asInt() > 25) {
+                System.out.println(node.toString());
+            }
+        }
+    }
+}
