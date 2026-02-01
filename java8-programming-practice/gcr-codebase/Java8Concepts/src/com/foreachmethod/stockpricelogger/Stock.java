520aacf453d3878166b2eecba3e2a452123cce2a
Aryan <borkararyan03@gmail.com>
2026-02-01 17:25:52 +0530

    [Aryan added : Stock]

---
+++ java8-programming-practice/gcr-codebase/Java8Concepts/src/com/foreachmethod/stockpricelogger/Stock.java
@@ -0,0 +1,27 @@
+package com.foreachmethod.stockpricelogger;
+
+public class Stock {
+	private String symbol;
+	private double price;
+	
+	public Stock( String symbol, double price )
+	{
+		this.symbol = symbol;
+		this.price = price;
+	}
+	
+	public String getSymbol()
+	{
+		return symbol;
+	}
+	public double getPrice()
+	{
+		return price;
+	}
+	
+	@Override
+	public String toString()
+	{
+		return String.format("[%s] Trading at $%.2f", symbol, price);
+	}
+}
