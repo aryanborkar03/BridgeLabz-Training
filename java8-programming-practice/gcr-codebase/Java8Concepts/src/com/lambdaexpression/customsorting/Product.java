8d435828438874590d15103398b01fe9db8ea716
Aryan <borkararyan03@gmail.com>
2026-02-01 18:20:35 +0530

    [Aryan added : Product]

---
+++ java8-programming-practice/gcr-codebase/Java8Concepts/src/com/lambdaexpression/customsorting/Product.java
@@ -0,0 +1,15 @@
+package com.lambdaexpression.customsorting;
+
+public class Product {
+	String name;
+	double price;
+	double ratings;
+	double discount;
+	
+	Product(String name, double price, double rating, double discount){
+		this.name = name;
+		this.price =price;
+		this.ratings = rating;
+		this.discount = discount;
+	}
+}
