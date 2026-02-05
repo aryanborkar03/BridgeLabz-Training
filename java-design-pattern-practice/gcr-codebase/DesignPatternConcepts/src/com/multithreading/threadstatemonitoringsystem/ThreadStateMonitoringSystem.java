23c546a58468041ccf48a0e8c491f2f5d8f2f887
Aryan <borkararyan03@gmail.com>
2026-02-05 13:27:24 +0530

    [Aryan added : ThreadStateMonitoringSystem]

---
+++ java-design-pattern-practice/gcr-codebase/DesignPatternConcepts/src/com/multithreading/threadstatemonitoringsystem/ThreadStateMonitoringSystem.java
@@ -0,0 +1,15 @@
+package com.multithreading.threadstatemonitoringsystem;
+class ThreadStateMonitoringSystem {
+    public static void main(String[] args) {
+
+        TaskRunner t1 = new TaskRunner("Task-1");
+        TaskRunner t2 = new TaskRunner("Task-2");
+
+        StateMonitor monitor = new StateMonitor(t1, t2);
+
+        monitor.start();
+
+        t1.start();
+        t2.start();
+    }
+}
