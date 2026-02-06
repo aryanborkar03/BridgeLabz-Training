8c295e848b18e32a1c572c2340e828f415dad4d2
Aryan <borkararyan03@gmail.com>
2026-02-06 13:33:19 +0530

    [Aryan added : ProductSort]

---
+++ java8-programming-practice/scenario-based-codebase/java8_scenario_based/src/com/lamdaexpresions/shoppingecommercesystem/ProductSort.java
@@ -0,0 +1,10 @@
+package com.lamdaexpresions.shoppingecommercesystem;
+
+import java.util.*;
+
+public class ProductSort {
+
+    public static void sortByPrice(List<Product> list) {
+        list.sort((p1, p2) -> Double.compare(p1.price, p2.price));
+    }
+}
